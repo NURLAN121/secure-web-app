a1bb8e47c8c621e840efc3a92eb8c866f0d08afa
Nurlanzada <atakinu5284_aehit@students.vizja.pl>
2026-01-08 01:16:25 +0100

    Lab 10: add basic Spring Security configuration

---
+++ src/main/java/com/example/securewebapp/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.securewebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // default login page
+                .formLogin(Customizer.withDefaults())
+
+
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
